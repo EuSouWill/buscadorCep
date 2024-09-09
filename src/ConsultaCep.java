c4a2dde5515ff4fe28a28f93806a00c5733a9ba0
Eusouwill <williamnunesregis@icloud.com>
2024-09-09 20:40:11 -0300

    buscador cep

---
+++ src/ConsultaCep.java
@@ -0,0 +1,30 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaCep {
+
+    public Endereco buscaEndereco(String cep) {
+        URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(endereco)
+                .build();
+
+
+        HttpResponse<String> response;
+        try {
+            response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Não conseguiu obter o endereço a partir desse cep", e);
+        }
+
+        return new Gson().fromJson(response.body(), Endereco.class);
+    }
+}
